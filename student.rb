df0538671aefa2dbc2e853468b463c3c6123f9b5
OsmanFP <osmanwako411@gmail.com>
2023-06-28 00:42:30 +0300

    Add classes

---
+++ student.rb
@@ -0,0 +1,13 @@
+class Student < Person
+    attr_accessor :classroom
+  
+    def initialize(name = 'Unknown', age = nil, parent_permission = true, classroom = nil)
+      super(name, age, parent_permission)
+      @classroom = classroom
+    end
+  
+    def play_hooky
+      '¯\\(ツ)/¯'
+    end
+  end
+  
---
+++ teacher.rb
@@ -0,0 +1,13 @@
+class Teacher < Person
+    attr_accessor :specialization
+  
+    def initialize(name = 'Unknown', age = nil, parent_permission = true, specialization = nil)
+      super(name, age, parent_permission)
+      @specialization = specialization
+    end
+  
+    def can_use_services?
+      true
+    end
+  end
+  
