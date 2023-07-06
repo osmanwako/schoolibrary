75dc1b9fb9d00f13fb6ad258aefbb0c0ea45a23c
massa <zoumaniguimassa12@gmail.com>
2023-07-06 08:03:16 +0000

    Adding select person and book files

--- createrental.rb
+++ createrental.rb
@@ -1,27 +1,3 @@
-def selectbook
-    books = APP.getbooks
-    return nil unless books
-  
-    puts('Select a book from the following list numbers')
-    books.each_with_index do |book, i|
-      puts("#{i}) Title : #{book.title}, Author:#{book.author}")
-    end
-    i = gets.chomp.to_i
-    books[i]
-end
-
-def selectperson
-    persons = APP.getpersons
-    return nil unless persons
-  
-    puts('Select a book from the following list numbers')
-    persons.each_with_index do |person, i|
-      puts("#{i}) Name : #{person.name}, ID:#{person.id}, Age: #{person.age}")
-    end
-    i = gets.chomp.to_i
-    persons[i]
-end
-
 def createrental
   book = selectbook
   person = selectperson
@@ -33,3 +9,4 @@ def createrental
   APP.addrental(rental)
   puts 'Rental created successfully!'
 end
+
